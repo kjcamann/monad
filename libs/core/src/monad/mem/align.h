27102c685553ed3a843e67b638a820767cb7cb85
Ken Camann <ken@monad.xyz>
2024-10-07 12:19:11 -0400

    [cma] debug assert that align is a power of 2 and re-use bit_round_up

--- libs/core/src/monad/mem/align.h
+++ libs/core/src/monad/mem/align.h
@@ -6,15 +6,28 @@
  * Provides a C version of the C++11 std::align
  */
 
+#include <stdbit.h>
 #include <stddef.h>
 
 #include <monad/core/assert.h>
+#include <monad/core/bit_util.h>
 #include <monad/core/likely.h>
+
+[[gnu::always_inline]] static inline size_t
+monad_round_size_to_align(size_t size, size_t align)
+{
+    // Round size up to the nearest multiple of align. bit_round_up does this,
+    // provided that align has the form 2^b and is expressed as `b`. `b` (which
+    // is `log_2 align`) is computed efficiently using stdc_trailing_zeros,
+    // an intrinsic operation on many platforms (e.g., TZCNT).
+    MONAD_DEBUG_ASSERT(stdc_has_single_bit(align));
+    return bit_round_up(size, stdc_trailing_zeros(align));
+}
 
 static inline uintptr_t
 monad_align(size_t align, size_t size, uintptr_t addr, size_t *space)
 {
-    uintptr_t const aligned_addr = (addr + (align - 1)) & ~(align - 1);
+    uintptr_t const aligned_addr = monad_round_size_to_align(addr, align);
     size_t const used = aligned_addr - addr + size;
     MONAD_DEBUG_ASSERT(space != nullptr);
     if (MONAD_UNLIKELY(used > *space)) {
@@ -22,9 +35,4 @@ monad_align(size_t align, size_t size, uintptr_t addr, size_t *space)
     }
     *space -= used;
     return aligned_addr;
-}
-
-static inline size_t monad_round_size_to_align(size_t size, size_t align)
-{
-    return (size + (align - 1)) & ~(align - 1);
 }
