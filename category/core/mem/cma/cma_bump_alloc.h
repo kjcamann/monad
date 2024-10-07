c2e0c47a6afee11bcee8e6a832efd7400858dd7d
Ken Camann <ken@monad.xyz>
2024-10-07 17:36:43 -0400

    [cma][mem][core] add realloc support
    This adds realloc and reallocarray user-facing functions, and
    implementations for the three existing allocators. This adds a test
    case for the bump pointer realloc, as it has quite a few corner
    cases

--- category/core/mem/cma/cma_bump_alloc.h
+++ category/core/mem/cma/cma_bump_alloc.h
@@ -14,16 +14,16 @@ struct monad_cma_bump_alloc
 {
     monad_allocator_t self;
     atomic_uintptr_t next;
-    uint8_t *begin;
-    uint8_t *end;
-    monad_memblk_t last;
+    uint8_t const *begin;
+    uint8_t const *end;
 };
 
 /// Initialize a "bump pointer allocator", which allocates from a fixed-size
 /// pre-allocated block by incrementing a pointer; called "stack allocator"
 /// in Alexandrescu's design, since the block is usually on the stack
 int monad_cma_bump_alloc_init(
-    struct monad_cma_bump_alloc *ma, monad_memblk_t blk);
+    struct monad_cma_bump_alloc *bump_alloc, monad_memblk_t blk,
+    monad_allocator_t **ma);
 
 #ifdef __cplusplus
 } // extern "C"
--- category/core/mem/cma/cma_fallback.c
+++ category/core/mem/cma/cma_fallback.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <string.h>
 
 #include <monad/core/likely.h>
 #include <monad/mem/cma/cma_alloc.h>
@@ -19,6 +20,41 @@ static int fallback_alloc(
     return rc;
 }
 
+static int cross_alloc_move(
+    monad_allocator_t *from, monad_allocator_t *to, size_t size, size_t align,
+    monad_memblk_t *old_blk)
+{
+    int rc;
+    monad_memblk_t new_blk;
+
+    rc = to->vtable->alloc(to, size, align, &new_blk);
+    if (rc != 0) {
+        return rc;
+    }
+    memcpy(
+        new_blk.ptr, old_blk->ptr, size > old_blk->size ? size : old_blk->size);
+    from->vtable->dealloc(from, *old_blk);
+    *old_blk = new_blk;
+    return 0;
+}
+
+static int fallback_realloc(
+    struct monad_cma_fallback_alloc *ma, size_t size, size_t align,
+    monad_memblk_t *blk)
+{
+    if (MONAD_UNLIKELY(blk == nullptr)) {
+        return EFAULT;
+    }
+    // For the rationale here, see Alexandrescu's original implementation in
+    // `fallback_allocator.d`
+    if (blk->ptr == nullptr || ma->primary->vtable->owns(ma->primary, *blk)) {
+        return ma->primary->vtable->realloc(ma->primary, size, align, blk) ||
+               cross_alloc_move(ma->primary, ma->secondary, size, align, blk);
+    }
+    return ma->secondary->vtable->realloc(ma->secondary, size, align, blk) ||
+           cross_alloc_move(ma->secondary, ma->primary, size, align, blk);
+}
+
 static void
 fallback_dealloc(struct monad_cma_fallback_alloc *ma, monad_memblk_t blk)
 {
@@ -37,41 +73,48 @@ fallback_owns(struct monad_cma_fallback_alloc *ma, monad_memblk_t blk)
 
 static struct monad_allocator_ops g_fallback_alloc_owns_ops = {
     .alloc = (monad_cma_alloc_fn *)fallback_alloc,
+    .realloc = (monad_cma_realloc_fn *)fallback_realloc,
     .dealloc = (monad_cma_dealloc_fn *)fallback_dealloc,
     .owns = (monad_cma_owns_fn *)fallback_owns};
 
 static struct monad_allocator_ops g_fallback_alloc_no_owns_ops = {
     .alloc = (monad_cma_alloc_fn *)fallback_alloc,
+    .realloc = (monad_cma_realloc_fn *)fallback_realloc,
     .dealloc = (monad_cma_dealloc_fn *)fallback_dealloc,
     .owns = nullptr};
 
 int monad_cma_fallback_alloc_init(
-    struct monad_cma_fallback_alloc *ma, monad_allocator_t *primary,
-    monad_allocator_t *secondary)
+    struct monad_cma_fallback_alloc *fallback, monad_allocator_t *primary,
+    monad_allocator_t *secondary, monad_allocator_t **ma)
 {
-    if (MONAD_UNLIKELY(ma == nullptr)) {
+    if (MONAD_UNLIKELY(fallback == nullptr)) {
         return EFAULT;
     }
     if (MONAD_UNLIKELY(primary == secondary)) {
         return EINVAL;
     }
-    ma->primary = primary;
-    if (ma->primary == nullptr) {
-        ma->primary = monad_cma_get_default_allocator();
+    fallback->primary = primary;
+    if (fallback->primary == nullptr) {
+        fallback->primary = monad_cma_get_default_allocator();
     }
-    if (MONAD_UNLIKELY(ma->primary->vtable->owns == nullptr)) {
-        return EOPNOTSUPP;
+    if (MONAD_UNLIKELY(fallback->primary->vtable->owns == nullptr)) {
+        return ENOTSUP;
     }
-    ma->secondary = secondary;
-    if (ma->secondary == nullptr) {
-        ma->secondary = monad_cma_get_default_allocator();
+    fallback->secondary = secondary;
+    if (fallback->secondary == nullptr) {
+        fallback->secondary = monad_cma_get_default_allocator();
     }
-    if (ma->primary->vtable->owns != nullptr &&
-        ma->secondary->vtable->owns != nullptr) {
-        ma->self.vtable = &g_fallback_alloc_owns_ops;
+    // The fallback allocator cannot function unless the primary allocator
+    // supports the "owns" operation. However, the fallback itself cannot
+    // support "owns" unless both of its allocators support it.
+    if (fallback->secondary->vtable->owns != nullptr) {
+        fallback->self.vtable = &g_fallback_alloc_owns_ops;
     }
     else {
-        ma->self.vtable = &g_fallback_alloc_no_owns_ops;
+        fallback->self.vtable = &g_fallback_alloc_no_owns_ops;
+    }
+    if (ma != nullptr) {
+        *ma = &fallback->self;
     }
     return 0;
 }
