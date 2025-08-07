9708345505e82ef671f73eaa7a3a5b60295f1a2c
Ken Camann <kcamann@category.xyz>
2025-08-07 18:36:35 -0400

    Add GPLv3 license to generated files

--- util/ctypes/codegen/c_codegen_common.py
+++ util/ctypes/codegen/c_codegen_common.py
@@ -19,6 +19,22 @@ C_CODEGEN_WARNING_FORMAT = \
 //
 {0}"""
 
+GPL_V3_LICENSE_COMMENT = \
+"""// Copyright (C) 2025 Category Labs, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>."""
+
 def create_command_comment() -> str:
   """Create a comment that describes how the code generator was run; this
   is written into the prologue replacing the `{0}` format element"""
