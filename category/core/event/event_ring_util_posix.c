62a92ecce2a95f499ac23d6e762f31b1f9e46ff5
Ken Camann <kcamann@category.xyz>
2025-09-02 15:53:52 -0400

    [event] move Linux-dependent functions to their own file
    Most of the event ring library is simple C23 and atomic operations, a
    few parts require Linux-specific behavior. Although the library isn't
    supported anywhere else, making it compile as if it was helps some
    Rust workflows, e.g., building the cargo docs locally on macOS will
    need to compile it.

---
+++ category/core/event/event_ring_util_posix.c
@@ -0,0 +1,25 @@
+#include <errno.h>
+
+#include <category/core/event/event_ring_util.h>
+#include <category/core/format_err.h>
+
+// Defined in event_ring.c, so we can share monad_event_ring_get_last_error()
+extern thread_local char _g_monad_event_ring_error_buf[1024];
+
+#define FORMAT_ERRC(...)                                                       \
+    monad_format_err(                                                          \
+        _g_monad_event_ring_error_buf,                                         \
+        sizeof(_g_monad_event_ring_error_buf),                                 \
+        &MONAD_SOURCE_LOCATION_CURRENT(),                                      \
+        __VA_ARGS__)
+
+int monad_event_ring_find_writer_pids(int, pid_t *, size_t *)
+{
+    return FORMAT_ERRC(ENOSYS, "function not available on non-Linux platforms");
+}
+
+int monad_check_path_supports_map_hugetlb(char const *, bool *supported)
+{
+    *supported = false;
+    return 0;
+}
