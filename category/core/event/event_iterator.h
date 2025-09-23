bef8b2c8d48bfe46b3cb79569f83dae0b2b33f17
Ken Camann <kcamann@category.xyz>
2025-09-23 18:46:10 -0400

    wip more changes to event_{ring,iterator}

--- category/core/event/event_iterator.h
+++ category/core/event/event_iterator.h
@@ -39,9 +39,15 @@ struct monad_event_iterator;
 struct monad_event_ring_control;
 
 /// Copy the next event descriptor and advance the iterator, if the next event
-/// is available; returns MONAD_EVENT_SUCCESS upon success, otherwise returns
-/// a code indicating why the iterator could not be advanced
+/// is available, similar to `*++i`; returns MONAD_EVENT_SUCCESS upon success,
+/// otherwise returns a code indicating why the iterator could not be advanced
 static enum monad_event_ring_result monad_event_iterator_try_next(
+    struct monad_event_iterator *, struct monad_event_descriptor *);
+
+/// Move the iterator back to previous event descriptor and copy it out, if the
+/// previous event is still available, similar to `*--i`; return behavior is
+/// the same as monad_event_iterator_try_next
+static enum monad_event_ring_result monad_event_iterator_try_prev(
     struct monad_event_iterator *, struct monad_event_descriptor *);
 
 /// Copy the event descriptor at the current iteration point, without advancing
@@ -49,6 +55,14 @@ static enum monad_event_ring_result monad_event_iterator_try_next(
 /// a code indicating why the descriptor at the iteration point was not ready
 static enum monad_event_ring_result monad_event_iterator_try_copy(
     struct monad_event_iterator const *, struct monad_event_descriptor *);
+
+/// Unchecked advance of the iterator by a signed distance; the typical
+/// iteration pattern is to use monad_event_iterator_try_next, which performs
+/// `*i++` atomically if and only if an event is ready, but the caller can also
+/// use `monad_event_iterator_try_copy`/ and `monad_event_iterator_advance`
+/// (which is unchecked) in certain situations
+static uint64_t
+monad_event_iterator_advance(struct monad_event_iterator *, int64_t distance);
 
 /// Set the iterator so that the next call to `monad_event_iterator_try_next`
 /// or `monad_event_iterator_try_copy` will read the event descriptor with the
@@ -86,12 +100,40 @@ inline enum monad_event_ring_result monad_event_iterator_try_next(
     return r;
 }
 
+inline enum monad_event_ring_result monad_event_iterator_try_prev(
+    struct monad_event_iterator *iter, struct monad_event_descriptor *event)
+{
+    if (MONAD_UNLIKELY(iter->read_last_seqno == 0)) {
+        return MONAD_EVENT_NOT_READY;
+    }
+    --iter->read_last_seqno;
+    enum monad_event_ring_result const r =
+        monad_event_iterator_try_copy(iter, event);
+    if (MONAD_UNLIKELY(r != MONAD_EVENT_SUCCESS)) {
+        ++iter->read_last_seqno;
+    }
+    return r;
+}
+
 inline enum monad_event_ring_result monad_event_iterator_try_copy(
     struct monad_event_iterator const *iter,
     struct monad_event_descriptor *event)
 {
     return monad_event_ring_try_copy(
         iter->event_ring, iter->read_last_seqno, event);
+}
+
+inline uint64_t monad_event_iterator_advance(
+    struct monad_event_iterator *iter, int64_t distance)
+{
+    int64_t const new_position = (int64_t)iter->read_last_seqno + distance;
+    if (MONAD_UNLIKELY(new_position < 0)) {
+        iter->read_last_seqno = 0;
+    }
+    else {
+        iter->read_last_seqno = (uint64_t)new_position;
+    }
+    return iter->read_last_seqno;
 }
 
 inline void monad_event_iterator_set_seqno(
