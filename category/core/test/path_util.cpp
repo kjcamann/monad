c85e92f1814174a4c018d66e00a3f02a7c860ec0
Ken Camann <kcamann@category.xyz>
2025-09-28 18:15:11 -0400

    [core] add path_util.h, make hugetlb_path.h use it
    This carves out the internal utility functions `walk_path_suffix`
    and `path_append` from `monad_hugetlbfs_open_dir_fd` so that they
    can be re-used elsewhere in the project.
    
    They are now called `monad_path_open_subdir` (enhanced form of the
    old `walk_path_suffix`) and `monad_path_append` (old `path_append`).
    `monad_hugetlbfs_open_dir_fd` is also rewritten to use these, and
    `monad_path_open_subdir` gets a unit test. `monad_path_append` is
    more robust and more useful than it was before.
    
    `monad_path_open_subdir` is used when you want to open a path to
    a subdirectory underneath some known existing directory hierarchy,
    but you do not know if the intermediate path components exist or
    not. You may want to do `mkdir -p` programatically to create them,
    or even if not, you'll want a better error message than open(2)
    would give you (just a mysterious ENOENT with no hint about what
    is wrong).
    
    "Known" existing directory is used losely here, since init_dirfd
    can be the special value AT_FDCWD to use the current working
    directory, and if `path_suffix` is absolute (contains a leading '/'),
    it will relative to root in any case -- both of which are the defined
    POSIX behavior of openat(2).
    
    `monad_path_open_subdir` is reused outside of the hugetblfs path
    calculation in a later event SDK commit.

---
+++ category/core/test/path_util.cpp
@@ -0,0 +1,78 @@
+// Copyright (C) 2025 Category Labs, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <print>
+
+#include <gtest/gtest.h>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/limits.h>
+#include <unistd.h>
+
+#include <category/core/path_util.h>
+
+TEST(PathUtil, Basic)
+{
+    int rc;
+    int dirfd;
+    char pathbuf[PATH_MAX];
+    constexpr char const TEST_DIR[] = "/tmp/monad-path-util-test/xyz";
+    constexpr char const PARENT_TEST_DIR[] = "/tmp/monad-path-util-test";
+
+    rc = monad_path_open_subdir(
+        AT_FDCWD,
+        TEST_DIR,
+        MONAD_PATH_NO_CREATE,
+        &dirfd,
+        pathbuf,
+        sizeof pathbuf);
+    ASSERT_EQ(rc, ENOENT); // Can't create the suffix, and it doesn't exist
+    ASSERT_EQ(dirfd, -1);
+
+    // Try again, this time we can create it
+    rc = monad_path_open_subdir(
+        AT_FDCWD, TEST_DIR, S_IRWXU, &dirfd, pathbuf, sizeof pathbuf);
+    ASSERT_EQ(rc, 0);
+    ASSERT_NE(dirfd, -1);
+    std::println(stderr, "full path is: {}", pathbuf);
+    (void)close(dirfd);
+
+    // Try again; we can't create it, but that's OK: it's there now
+    rc = monad_path_open_subdir(
+        AT_FDCWD,
+        TEST_DIR,
+        MONAD_PATH_NO_CREATE,
+        &dirfd,
+        pathbuf,
+        sizeof pathbuf);
+    ASSERT_EQ(rc, 0);
+    ASSERT_NE(dirfd, -1);
+    (void)close(dirfd);
+
+    // Remove the test directory
+    ASSERT_EQ(rmdir(TEST_DIR), 0);
+    ASSERT_EQ(rmdir(PARENT_TEST_DIR), 0);
+
+    // Either parameter can be nullptr
+    rc = monad_path_open_subdir(
+        AT_FDCWD,
+        TEST_DIR,
+        MONAD_PATH_NO_CREATE,
+        nullptr,
+        nullptr,
+        sizeof pathbuf);
+    ASSERT_EQ(rc, ENOENT);
+}
