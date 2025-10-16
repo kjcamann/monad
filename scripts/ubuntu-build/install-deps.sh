24611b5e6651b12a2243958cdd1c1b94c6930b83
Alexander Lee <alee@category.xyz>
2025-10-16 14:10:05 -0400

    move apt install to scripts

--- scripts/ubuntu-build/install-deps.sh
+++ scripts/ubuntu-build/install-deps.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 packages=(
+  libarchive-dev
   libbenchmark-dev
+  libbrotli-dev
+  libcap-dev
   libcgroup-dev
+  libcli11-dev
   libcrypto++-dev
   libgmock-dev
+  libgmp-dev
   libgtest-dev
   libhugetlbfs-dev
   libtbb-dev
   liburing-dev
+  libzstd-dev
 )
 
 apt install -y "${packages[@]}"
--- scripts/ubuntu-build/install-tools.sh
+++ scripts/ubuntu-build/install-tools.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
 packages=(
+  apt-utils
+  ca-certificates
+  clang-19
   clang-tools-19
   clang-tidy-19
   cmake
+  curl
+  dialog
+  g++-15
+  gcc-15
   gdb
   git
+  gnupg
   libhugetlbfs-bin
   ninja-build
   pkg-config
   python-is-python3
   python3-pytest
+  software-properties-common
   valgrind
+  wget
 )
 
 apt install -y "${packages[@]}"
