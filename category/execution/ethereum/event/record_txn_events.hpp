c4f14d47f55dfa215126425829f1e9aa4aef7a57
Ken Camann <kcamann@category.xyz>
2025-11-10 20:07:41 -0500

    [event] record state access events
    There are two changes here:
    
    - record_txn_events.cpp gains functions for recording State objects
      as `ACCOUNT_ACCESS_LIST_HEADER`, `ACCOUNT_ACCESS`, and
      `STORAGE_ACCESS` events
    
    - All state-affecting changes to the block that occur before
      transaction and after the transaction are also recorded

--- category/execution/ethereum/event/record_txn_events.hpp
+++ category/execution/ethereum/event/record_txn_events.hpp
@@ -23,11 +23,15 @@
 #include <optional>
 #include <span>
 
+enum monad_exec_account_access_context : uint8_t;
+
 MONAD_NAMESPACE_BEGIN
 
 struct CallFrame;
 struct Receipt;
 struct Transaction;
+
+class State;
 
 /// Record the transaction header events (TXN_HEADER_START, the EIP-2930
 /// and EIP-7702 events, and TXN_HEADER_END)
@@ -38,11 +42,17 @@ void record_txn_header_events(
 /// Record TXN_EVM_OUTPUT, and all subsequent execution output events
 /// (TXN_LOG, TXN_CALL_FRAME, etc.)
 void record_txn_output_events(
-    uint32_t txn_num, Receipt const &, std::span<CallFrame const>);
+    uint32_t txn_num, Receipt const &, std::span<CallFrame const>,
+    State const &);
 
 /// Record TXN_REJECT or EVM_ERROR events depending on what happened during
 /// transaction execution
 void record_txn_error_event(
     uint32_t txn_num, Result<Receipt>::error_type const &);
+
+/// Record all account state accesses (both reads and writes) described by a
+/// State object
+void record_account_access_events(
+    monad_exec_account_access_context, State const &);
 
 MONAD_NAMESPACE_END
