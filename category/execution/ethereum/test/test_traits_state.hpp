815224b3de683cbd43dd920c608ed69a1fded06a
James Hunsaker <18537138+jhunsaker@users.noreply.github.com>
2025-12-19 14:44:35 -0500

    add state traits test

---
+++ category/execution/ethereum/test/test_traits_state.hpp
@@ -0,0 +1,49 @@
+// Copyright (C) 2025 Category Labs, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <category/execution/ethereum/db/trie_db.hpp>
+#include <category/execution/ethereum/db/util.hpp>
+#include <category/execution/ethereum/state2/block_state.hpp>
+#include <category/execution/ethereum/state3/state.hpp>
+#include <category/mpt/db.hpp>
+#include <category/vm/vm.hpp>
+
+#include <monad/test/traits_test.hpp>
+
+MONAD_NAMESPACE_BEGIN
+
+struct InMemoryStateTestBase
+{
+    InMemoryMachine machine;
+    mpt::Db db{machine};
+    TrieDb tdb{db};
+    vm::VM vm;
+    BlockState block_state{tdb, vm};
+    State state{block_state, Incarnation{0, 0}};
+};
+
+template <typename T>
+struct InMemoryStateTraitsTest
+    : public InMemoryStateTestBase
+    , public TraitsTest<T>
+{
+    using InMemoryStateTestBase::state;
+};
+
+DEFINE_TRAITS_FIXTURE(InMemoryStateTraitsTest);
+
+MONAD_NAMESPACE_END
