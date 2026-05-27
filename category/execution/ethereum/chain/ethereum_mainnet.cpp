00dc6467e0bad89e0dc7f71700ed9391f198282d
Craig McLaughlin <cmclaughlin@category.xyz>
2026-05-27 21:41:13 +0100

    [execution/ethereum] Activate Prague on mainnet
    Return EVMC_PRAGUE for Ethereum mainnet blocks at or after the
    Prague/Pectra activation timestamp.

--- category/execution/ethereum/chain/ethereum_mainnet.cpp
+++ category/execution/ethereum/chain/ethereum_mainnet.cpp
@@ -46,9 +46,10 @@ uint256_t EthereumMainnet::get_chain_id() const
 evmc_revision EthereumMainnet::get_revision(
     uint64_t const block_number, uint64_t const timestamp) const
 {
-    // TODO: update to include Prague once we can replay those blocks
-
-    if (MONAD_LIKELY(timestamp >= 1710338135)) {
+    if (MONAD_LIKELY(timestamp >= 1746612311)) {
+        return EVMC_PRAGUE;
+    }
+    else if (timestamp >= 1710338135) {
         return EVMC_CANCUN;
     }
     else if (timestamp >= 1681338455) {
